4b6a10cf015a639a860285eb23d629c905525128
ahidkbro <enzo11370@hotmail.fr>
2026-02-07 20:28:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let noBtn = document.getElementById("no");
+let yesBtn = document.getElementById("yes");
+let question = document.getElementById("question");
+
+let noSize = 1;
+let yesSize = 1;
+
+let phrases = [
+  "Euh… tu t'es trompée là 🤨",
+  "Wsh c’est bizarre… ça bug non ? 🤔",
+  "MAIS PTN DIS OUI 😡💔"
+];
+
+let clickCount = 0;
+
+noBtn.addEventListener("click", () => {
+  noSize -= 0.1;
+  yesSize += 0.1;
+
+  noBtn.style.transform = `scale(${noSize})`;
+  yesBtn.style.transform = `scale(${yesSize})`;
+
+  if (clickCount < phrases.length) {
+    question.textContent = phrases[clickCount];
+    clickCount++;
+  }
+});
+
+yesBtn.addEventListener("click", () => {
+  alert("Bonne réponse 😌💖 (la suite arrive)");
+});
